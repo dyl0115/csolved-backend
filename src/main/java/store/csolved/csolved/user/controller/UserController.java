0d2f381441df47f340281e0fad941bfa6a10919e
doubleDRG <dyl660115@gmail.com>
2024-12-22 20:16:28 +0900

    test: DB Test

--- src/main/java/store/csolved/csolved/user/controller/UserController.java
+++ src/main/java/store/csolved/csolved/user/controller/UserController.java
@@ -1,14 +1,28 @@
 package store.csolved.csolved.user.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController
 {
-    @GetMapping("/")
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @GetMapping("/db-check")
     public String hello()
     {
-        return "hello";
+        try
+        {
+            // 테스트 데이터 삽입 시도
+            jdbcTemplate.execute("INSERT INTO test_table (name) VALUES ('test-data-" + System.currentTimeMillis() + "')");
+            return "DB 저장 성공!";
+        }
+        catch (Exception e)
+        {
+            return "DB 에러: " + e.getMessage();
+        }
     }
 }
