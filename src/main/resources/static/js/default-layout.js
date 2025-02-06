3d53de446a78616550d4ef3dfdfcebeb6391da4a
doubleDRG <dyl660115@gmail.com>
2025-02-06 16:48:02 +0900

    feat: default-layout 부트스트랩 생성

---
+++ src/main/resources/static/js/default-layout.js
@@ -0,0 +1,35 @@
+/*!
+* Start Bootstrap - Bare v5.0.9 (https://startbootstrap.com/template/bare)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+function signOut() {
+    const form = document.createElement('form');
+    form.method = 'POST';
+    form.action = '/auth/signOut';
+    document.body.appendChild(form);
+    form.submit();
+}
+
+function withdraw() {
+
+    const form = document.createElement('form');
+    form.method = 'POST';
+    form.action = '/auth/withdraw';
+
+    const methodInput = document.createElement('input');
+    methodInput.type = 'hidden';
+    methodInput.name = '_method';
+    methodInput.value = 'DELETE';
+    form.appendChild(methodInput);
+
+    document.body.appendChild(form);
+    form.submit();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    lucide.createIcons();
+});
