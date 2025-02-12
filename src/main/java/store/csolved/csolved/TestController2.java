0186bcbbc518b9d780bff6c48206b33585159c35
doubleDRG <dyl660115@gmail.com>
2025-02-12 17:21:53 +0900

    save: 무중단 배포 전 저장

--- src/main/java/store/csolved/csolved/TestController2.java
+++ src/main/java/store/csolved/csolved/TestController2.java
@@ -17,6 +17,6 @@ public class TestController2
     public String testView(Model model)
     {
         model.addAttribute("user", User.builder().build());
-        return "create-update-layout";
+        return "create-update-layout"; 
     }
 }
