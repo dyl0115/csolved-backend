7754bdab73e306508d0046ba00f1f3a925e4ef97
doubleDRG <dyl660115@gmail.com>
2025-09-25 21:51:06 +0900

    [function] notice 테이블 생성

--- src/test/resources/db/schema/test-schema.sql
+++ src/test/resources/db/schema/test-schema.sql
@@ -170,3 +170,21 @@ CREATE TABLE reports
     PRIMARY KEY (id),
     CONSTRAINT reports_ibfk_1 FOREIGN KEY (reporter_id) REFERENCES users (id)
 );
+
+-- notice 테이블
+CREATE TABLE notice
+(
+    id          BIGINT PRIMARY KEY AUTO_INCREMENT,
+    title       VARCHAR(255) NOT NULL,
+    content     TEXT         NOT NULL,
+    author_id   BIGINT       NOT NULL REFERENCES User (id),
+
+    -- 공지사항 특화 컬럼들
+    is_pinned   BOOLEAN   DEFAULT FALSE, -- 상단 고정
+
+    -- 기본 관리
+    views       INTEGER   DEFAULT 0,     -- 조회수
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at  TIMESTAMP DEFAULT NULL
+);
